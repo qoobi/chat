811b49ac5ca22aa4b699ad8dd02fe2d22ce9b706
Mikhail Gilmutdinov <com@mmkg.me>
2017-03-21 11:30:03 +0300

    begin task 2

--- Tinkoff Chat/ViewController.swift
+++ Tinkoff Chat/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    private var tapCounter = 0
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var textColorLabel: UILabel!
+    @IBOutlet weak var loginTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(onTapAction))
+        view.addGestureRecognizer(tapGesture)
     }
 
+    func onTapAction() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == loginTextField {
+            view.endEditing(true)
+            return false
+        }
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func saveAction(_ sender: Any) {
+        guard sender is UIButton else { return }
+        print("This scene should be saved")
+    }
 
 }
 
